ecb62c639ebb19b6e34f9e236ae170502f0b07eb
Ruhul Amin <46115166+amihabibkhan@users.noreply.github.com>
2023-04-16 15:49:40 +0600

    Update youtube_url_to_id.php

--- youtube_url_to_id.php
+++ youtube_url_to_id.php
@@ -1,12 +1,17 @@
-public function urlToId($url)
-    {
-        $sbstr = substr($url, 32);
+function urlToId($url)
+{
+    $sbstr = substr($url, 32);
+    $sbstr_share = substr($url, 17);
 
-        if  (strrchr($sbstr, "&")){
-            $character_needed = strpos($sbstr, '&');
-            return substr($sbstr, 0, $character_needed);
-        }else{
-            return $sbstr;
-        }
-
+    if (substr($url, 0, 17) == "https://youtu.be/"){
+        $final_string = $sbstr_share;
+    } else{
+        $final_string = $sbstr;
+    } 
+    if  (strrchr($final_string, "&")){
+        $character_needed = strpos($final_string, '&');
+        return substr($final_string, 0, $character_needed);
+    }else{
+        return $final_string;
     }
+}
